2db4e5249b54ea2c92d577bed1443d6131a48dd8
HalkoZ <111779806+HalkoZ@users.noreply.github.com>
2023-02-03 15:10:20 +0100

    Quotes project final
    Quotes project final

---
+++ 23 - Quotes website/addnew.js
@@ -0,0 +1,40 @@
+var token = localStorage.getItem("auth_token");
+if (!token) {
+  window.location.href = "index.html";
+}
+
+document.getElementById("addNew").addEventListener("click", function () {
+  var text = document.getElementById("quoteText").value;
+  var author = document.getElementById("quoteAuthor").value;
+  var source = document.getElementById("quoteSource").value;
+
+  var newQuote = {
+    quoteText: text,
+    quoteAuthor: author,
+    quoteSource: source,
+  };
+  if (text != "" && author != "" && source != "") {
+    fetch("https://js-course-server.onrender.com/quotes/add-quote", {
+      method: "POST",
+      body: JSON.stringify(newQuote),
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        document.getElementById("quoteSource").value = "";
+        document.getElementById("quoteText").value = "";
+        document.getElementById("quoteAuthor").value = "";
+        alert("Vas citat je uspesno dodat");
+      })
+      .catch(function (error) {
+        console.log("error:", error);
+      });
+  } else {
+    alert("Morate popuniti sva polja");
+  }
+});
